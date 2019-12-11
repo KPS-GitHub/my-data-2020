c1d90b10b0288fa07d47a62c74ded11ee13771e2
Kennan Smith <kennan@watermarkadvertising.net>
2019-12-11 09:54:53 -0700

    basic layout in place

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      suh dude
-    </div>
+    <OuterWrap>
+      <MainWrap>
+        <StyledRow>
+          <StyledSection sm="12" lg="6">
+            Money
+          </StyledSection>
+          <StyledSection sm="12" lg="6">
+            Calories
+          </StyledSection>
+        </StyledRow>
+        <StyledRow>
+          <StyledSection sm="12" lg="6">
+            Goals
+          </StyledSection>
+          <StyledSection sm="12" lg="6">
+            Fun
+          </StyledSection>
+        </StyledRow>
+      </MainWrap>
+    </OuterWrap>
   );
 }
 
 export default App;
+
+
+const OuterWrap = styled.div`
+  margin: 20rem 20px 20px 20px;
+`
+
+const MainWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
+
+const StyledRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-bottom: 50px;
+`
+
+const StyledSection = styled.div`
+  width: 30vw;
+  height: 20rem;
+  background: red;
+  border: 5px solid black;
+  :hover {
+    transform: scale(1.5,1.5);
+    -webkit-transition: transform .5s;
+    transition: transform .5s;
+  }
+`
