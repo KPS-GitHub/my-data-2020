22f7438b342f9d2ab0f8a74173885e68c2d5f7b3
Kennan Smith <kennan@watermarkadvertising.net>
2019-12-27 11:47:42 -0700

    programattic rendering >>>>>>>>> hard-coded rendering

--- src/components/PreviewHomePage/Home.js
+++ src/components/PreviewHomePage/Home.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const pages = ["Money", "Calories", "Goals", "Fun"];
+
 const Home = () => {
   return (
     <OuterWrap>
       <MainWrap>
         <StyledRow>
-          <StyledSection>
-            Money
-          </StyledSection>
-          <Spacer />
-          <StyledSection>
-            Calories
-          </StyledSection>
-        </StyledRow>
-        <StyledRow>
-          <StyledSection>
-            Goals
-          </StyledSection>
-          <Spacer />
-          <StyledSection>
-            Fun
-          </StyledSection>
+          {pages.map(page => {
+            return(
+              <StyledSection href={`/`+page}>
+                  {page}
+              </StyledSection>
+            );
+          })}
         </StyledRow>
       </MainWrap>
     </OuterWrap>
@@ -48,16 +41,19 @@ const StyledRow = styled.div`
   margin-bottom: 50px;
 `
 
-const StyledSection = styled.div`
+const StyledSection = styled.a`
   position: relative;
   width: 90%;
   height: 20rem;
-  padding-bottom: .5rem;
+  margin: .5rem;
+  padding: .5rem;
   background: red;
   border: 5px solid black;
   transform(1,1);
   -webkit-transition: transform .5s;
   transition: transform .5s;
+  color: black;
+  text-decoration: none;
   :hover {
     transform: scale(1.1,1.2);
     -webkit-transition: transform .5s;
