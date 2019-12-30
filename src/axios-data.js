91ba4caad2d52e1a46b10809644326a954374fe8
Kennan Smith <kennan@watermarkadvertising.net>
2019-12-30 09:15:46 -0700

    did a gatsby new inside of this repo to get the gatsby conversion started

--- src/axios-data.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'https://mydata-822b0.firebaseio.com/',
-    'Access-Control-Allow-Origin': true
-});
-
-export default instance;
