d0dc785b29cb565dc6cf5a72099ae9bca3318960
Kennan Smith <kennan@watermarkadvertising.net>
2019-12-30 09:38:57 -0700

    trying to get netlify deploy to work

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,7 +8,7 @@ exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/kennansmith/Desktop/Temp_Task_Folder/my-data-2020/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/kennansmith/Desktop/Temp_Task_Folder/my-data-2020/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/kennansmith/Desktop/Temp_Task_Folder/my-data-2020/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/kennansmith/Desktop/Temp_Task_Folder/my-data-2020/src/pages/page-2.js"))),
-  "component---src-pages-money-js": hot(preferDefault(require("/Users/kennansmith/Desktop/Temp_Task_Folder/my-data-2020/src/pages/Money.js")))
+  "component---src-pages-money-js": hot(preferDefault(require("/Users/kennansmith/Desktop/Temp_Task_Folder/my-data-2020/src/pages/Money.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/kennansmith/Desktop/Temp_Task_Folder/my-data-2020/src/pages/page-2.js")))
 }
 
